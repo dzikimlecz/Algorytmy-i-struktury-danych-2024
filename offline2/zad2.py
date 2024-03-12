0559585c387ea2fb0fc9ba73d1af80dda85ebe34
dzikimlecz <j.pul312@gmail.com>
2024-03-12 22:20:39 +0100

    Implemented offline2 using quickSeklect (5/10)

--- offline2/zad2.py
+++ offline2/zad2.py
@@ -1,6 +1,15 @@
 from zad2testy import runtests
 
 
+class Node:
+    def __init__(self, val=None):
+        self.next = None
+        self.val = val
+
+    def __str__(self):
+        return '{' + self.val + '}'
+
+
 def part(thelist, start, end):
     pivot = thelist[end - 1]
     smallerPartEnd = start
@@ -12,15 +21,22 @@ def part(thelist, start, end):
     return smallerPartEnd
 
 
-
-def quickSort(thelist, start=0, end=None):
-    if end is None:
-        end = len(thelist)
-    if start < end:
-        partIndex = part(thelist, start, end)
-        quickSort(thelist, start=start, end=partIndex)
-        quickSort(thelist, start=partIndex+1, end=end)
-
+def quickSelect(thelist, k):
+    stack = Node((0, len(thelist)))
+    while stack.val:
+        start, end = stack.val
+        stack = stack.next
+        size = end - start
+        pivotIndex = part(thelist, start, end)
+        if len(thelist) - k == pivotIndex:
+            new = Node()
+        elif len(thelist) - k < pivotIndex:
+            new = Node((start, pivotIndex))
+        else:
+            new = Node((pivotIndex + 1, end))
+        new.next = stack
+        stack = new
+    return thelist[pivotIndex]
 
 
 def ksum(T, k, p):
@@ -29,8 +45,7 @@ def ksum(T, k, p):
     n = len(T)
     for i in range(0, n - p + 1):
         sub = T[i : i + p]
-        quickSort(sub)
-        ssum += sub[len(sub) - k]
+        ssum += quickSelect(sub, k)
     return ssum
 
 
