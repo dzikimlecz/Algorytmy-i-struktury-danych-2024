1795c45b89585e70794476311c2d73936efc1a0b
dzikimlecz <j.pul312@gmail.com>
2024-03-17 22:35:00 +0100

    Implemented offline2 using quicksort and binsearch

--- offline2/zad2.py
+++ offline2/zad2.py
@@ -2,166 +2,84 @@ from zad2testy import runtests
 
 # Jan Pulkowski
 # (420313)
-# Złożoność pamięciowa O(p+k), obliceniowa O(np)
-# Algorytm Selekcji k-tych największych elementów z użyciem kopców.
-# W pamięci przechowywane są 2 kopce, jeden sortujący rosnąco (kopiecMax), drugi malejąco(kopiecMin).
-# Początkowo do kopcaMax wczytywane jest p 1. elemenmtów listy,
-# następnie k pierwszych elementów kopcaMax jest zdejmowane na kopiecMin.
-# 1. element kopcamin to pierwsza k-ta największa wartość - dodaję ją do całościowej sumy.
-# Następnie dla pozostałych (p - 1) przedziałów
-# usuwam element bespośrednio za przedziałem z kopca w którym się znajdował,
-# a na jego miejsce wstawiam element końcowy nowego przedziału.
-# Odbudowuję kopce i ewentualnie zamieniam 1. elementy kopcaMin i kopcaMax
-# jeżeli ten z kopcaMax jest większy. Uruchamiam na obu procedurę heapify.
-# Szczyt kopca min ponownie zawiera k-ty największy element, dodaję go do sumy.
+# Złożoność pamięciowa O(p), obliczeniowa O(n * p^2)
+# Algorytm:
+# Biorę 1. p elementów listy, sortuję je.
+# Biorę k-ty największy z nich, dodaję do sumy.
+# Dla każdej kolejnej podlisty:
+# Binarnie wyszukuję element do usunięcia
+# i pozycję do wstawienia nowego elementu,
+# tak by podlista dalej była posortowana.
+# Biorę k-ty największy element podlisty, dodaję do sumy.
+# Po pętli zwracam sumę.
 
 
-def heapifyMin(hp, i, size):
-    while 2 * i < size:
-        smallest = i
-        left = 2 * i
-        right = 2 * i + 1
-        if hp[smallest] > hp[left]:
-            smallest = left
-        if right < size and hp[smallest] > hp[right]:
-            smallest = right
-        if smallest != i:
-            hp[i], hp[smallest] = hp[smallest], hp[i]
-            i = smallest
+def binSearch(t, val):
+    start = 0
+    end = len(t) - 1
+    while start != end:
+        test = (start + end) // 2 + (start + end) % 2
+        if t[test] > val:
+            end = test - 1
         else:
-            i = size
+            start = test
+    return start if t[start] == val else None
 
 
-def heapifyMax(hp, i, size):
-    while 2 * i < size:
-        largest = i
-        left = 2 * i
-        right = 2 * i + 1
-        if hp[largest] < hp[left]:
-            largest = left
-        if right < size and hp[largest] < hp[right]:
-            largest = right
-        if largest != i:
-            hp[i], hp[largest] = hp[largest], hp[i]
-            i = largest
+def posBinSearch(t, val):
+    if val <= t[0]:
+        return 0
+    if t[-1] <= val:
+        return len(t)
+    start = 1
+    end = len(t) - 1
+    while start != end:
+        test = (start + end) // 2 + (start + end) % 2
+        if t[test - 1] > val:
+            end = test - 1
         else:
-            i = size
+            start = test
+    return start if t[start - 1] <= val < t[start] else None
 
 
-def heapifyMaxUp(heap, i, size):
-    while i:
-        heapifyMax(heap, i, size)
-        i //= 2
-
-def heapifyMinUp(heap, i, size):
-    while i:
-        heapifyMin(heap, i, size)
-        i //= 2
-
-
-def buildHeapMin(heap):
-    size = len(heap)
-    for i in range(size // 2, 0 , -1):
-        heapifyMin(heap, i, size)
-
-
-def buildHeapMax(heap):
-    size = len(heap)
-    for i in range(size // 2, 0, -1):
-        heapifyMax(heap, i, size)
+def partition(T, start, end):
+    pivot = T[start]
+    left = start - 1
+    right = end + 1
+    while True:
+        while True:
+            left += 1
+            if T[left] >= pivot:
+                break
+        while True:
+            right -= 1
+            if T[right] <= pivot:
+                break
+        if left >= right:
+            return right
+        T[left], T[right] = T[right], T[left]
 
 
-def replaceMax(heap, size, drop, nxt):
-    if heap[1] == drop:
-        heap[1] = nxt
-        heapifyMax(heap, 1, size)
-        return
-    q = [0] * (size)
-    qSize = 1
-    qStart = 0
-    q[0] = 1
-    while qSize:
-        val = q[qStart]
-        qStart += 1
-        qSize -= 1
-        test = val * 2
-        for i in range(2):
-            if test < size:
-                if heap[test] == drop:
-                    heap[test] = nxt
-                    heapifyMaxUp(heap, test, size)
-                    return
-                if heap[test] > drop:
-                    q[qStart + qSize] = test
-                    qSize += 1
-            test += 1
+def quickSort(T, start, end):
+    while start >= 0 and end >= 0 and start < end:
+        partIndex = partition(T, start, end)
+        quickSort(T, start, partIndex)
+        start = partIndex + 1
 
-
-def findMin(heap, drop):
-    if heap[1] == drop:
-        return 1
-    q = [0] * (len(heap))
-    qSize = 1
-    qStart = 0
-    q[0] = 1
-    while qSize:
-        val = q[qStart]
-        qStart += 1
-        qSize -= 1
-        test = val * 2
-        for i in range(2):
-            if test < len(heap):
-                if heap[test] == drop:
-                    return test
-                if heap[test] < drop:
-                    q[qStart + qSize] = test
-                    qSize += 1
-            test += 1
-    return -1
-
-
-
-
-def heapSelect(t, k, p):
-    n = len(t)
-    minHeap = [0 for _ in range(k + 1)]
-    maxHeap = [0 for _ in range(p + 1)]
-    minHeap[0] = maxHeap[0] = None
-    for i in range(p):
-        maxHeap[i + 1] = t[i]
-    buildHeapMax(maxHeap)
-    maxSize = p + 1
-    for j in range(k):
-        pop = maxHeap[1]
-        maxHeap[1] = maxHeap[maxSize - 1]
-        maxSize -= 1
-        heapifyMax(maxHeap, 1, maxSize)
-        minHeap[j + 1] = pop
-    buildHeapMin(minHeap)
-    ssum = minHeap[1]
-    for i in range(n - p):
-        drop = t[i]
-        nxt = t[i + p]
-        if drop < minHeap[1]:
-            replaceMax(maxHeap, maxSize, drop, nxt)
-        else:
-            ix = findMin(minHeap, drop)
-            maxHeap[maxSize] = nxt
-            heapifyMaxUp(maxHeap, maxSize, maxSize + 1)
-            minHeap[ix] = maxHeap[1]
-            heapifyMinUp(minHeap, ix, len(minHeap))
-            maxHeap[1] = maxHeap[maxSize]
-            heapifyMax(maxHeap, 1, maxSize)
-        if minHeap[1] < maxHeap[1]:
-            minHeap[1], maxHeap[1] = maxHeap[1], minHeap[1]
-            heapifyMin(minHeap, 1, len(minHeap))
-            heapifyMax(maxHeap, 1, maxSize)
-        ssum += minHeap[1]
-    return ssum
 
 def ksum(T, k, p):
-    # tu prosze wpisac wlasna implementacje
-    return heapSelect(T, k, p)
+    n = len(T)
+    cp = T[0:p]
+    quickSort(cp, 0, p - 1)
+    ssum = cp[p - k]
+    for i in range(n - p):
+        ix = binSearch(cp, T[i])
+        del cp[ix]
+        new = T[i + p]
+        insertIx = posBinSearch(cp, new)
+        cp.insert(insertIx, new)
+        ssum += cp[p-k]
+    return ssum
 
-# zmien all_tests na True zeby uruchomic wszystkie testy
+
 runtests( ksum, all_tests=True )
