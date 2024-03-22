49feb6ed986fcb0ae1d5db255c6135633ca38597
dzikimlecz <j.pul312@gmail.com>
2024-03-22 22:30:30 +0100

    Implemented best solution for offline3.

--- offline3/zad3.py
+++ offline3/zad3.py
@@ -1,76 +1,48 @@
 from zad3testy import runtests
 
+# Jan Pulkowski
+# (420313)
+# Obliczam sumy prefiksowe liczby punktów położonych na danej wartości dla obu współrzędnej
+# Za pomocą sum prefiksowych wyznaczam dla każdego punktu różnicę między
+# ilością punktów dominowanych przez niego a dominujących go.
+# Element maksymalny nie będzie dominowany przez żaden punkt, więc wyliczona
+# dla niego wartość jest równa liczbie punktów dominowanych przez niego.
+# Lider zbioru będzie elementem maksymalnym o największej wyliczonej wartości.
+#
+# Wyżej wspomniana różnica jest wyliczana dla punktu p0 w następujący sposób
+# aDb <=> a[0] > b[0] and a[1] > b[1];
+# val = #{ p in P : p0 D p } - #{ p in P : p D p0 } =
+# = -(#{ p in P : p D p0 } + #{p0}) + #{ p in P : p0 D p } + 1 =
+# = - (|P| - #{ p in P : p[0] < p0[0] or p[1] < p0[1] }) + #{ p in P : p0 D p } + 1 =
+# = #{ p in P : p0 D p } + #{ p in P : p[0] < p0[0] or p[1] < p0[1]} - |P| + 1 =
+# = #{ p in P : p[0] < p0[0] and p[1] < p0[1] } + #{ p in P : p[0] < p0[0] or p[1] < p0[1] } - |P| + 1 =
+# = #{ p in P : p[0] < p0[0] } + #{ p in P : p[1] < p0[1] } - |P| + 1
+#
+# więc wartość jest wyliczana przez: prefixX[x - 1] + prefixY[y - 1] - len(P) + 1
 
-def cntingGetTemp(points: list, key: int, n: int) -> list:
-    temp = [[0] for _ in range(n + 1)]
-    for pt in points:
-        k = pt[key]
-        temp[k][0] += 1
-        temp[k].append(pt)
-    for i in range(1, len(temp)):
-        temp[i][0] += temp[i - 1][0]
-    return temp
-
-
-def countingSortPacking(points: list, key: int, n: int) -> list:
-    temp = cntingGetTemp(points, key, n)
-    result = []
-    for record in temp:
-        if len(record) > 1:
-            result.append(record[1:])
-    return result
-
-
-def countingSort(points: list, key: int, n: int) -> list:
-    temp = cntingGetTemp(points, key, n)
-    result = [0] * len(points)
-    for i in range(n, -1, -1):
-        record = temp[i]
-        while len(record) > 1:
-            record[0] -= 1
-            result[record[0]] = record.pop()
-    return result
-
-
-def radixSort(points: list) -> list:
-    n = len(points)
-    partSorted = countingSortPacking(points, 1, n)
-    for i in range(len(partSorted)):
-        partSorted[i] = countingSort(partSorted[i], 0, n)
-    return partSorted
+class Record:
+    def __init__(self):
+        self.pt = (-1,-1)
+        self.maximised = -1
 
 
 def dominance(P):
-    groupSorted = radixSort(P)
-    maximas = groupSorted[0]
-    for i in range(1, len(groupSorted)):
-        newMaximas = [e for e in groupSorted[i]]
-        maximasCursor = -1
-        ll = len(maximas)
-        for j in range(len(groupSorted[i])):
-            pt = groupSorted[i][j]
-            while maximasCursor + 1 < ll and pt[0] >= maximas[maximasCursor + 1][0]:
-                maximasCursor += 1
-        maximasCursor += 1
-        if maximasCursor < ll:
-            newMaximas += maximas[maximasCursor:]
-        maximas = newMaximas
-    maxDom = 0
-    for maximum in maximas:
-        i = 0
-        j = 0
-        dom = 0
-        while i < len(groupSorted) and maximum[1] > groupSorted[i][j][1]:
-            while j < len(groupSorted[i]) and maximum[0] > groupSorted[i][j][0]:
-                dom += 1
-                j += 1
-            j = 0
-            i += 1
-        maxDom = max(maxDom, dom)
-    return maxDom
-
+    prefixX = [0] * (len(P) + 1)
+    prefixY = [0] * (len(P) + 1)
+    for pt in P:
+        prefixX[pt[0]] += 1
+        prefixY[pt[1]] += 1
+    for i in range(1, len(P) + 1):
+        prefixX[i] += prefixX[i - 1]
+        prefixY[i] += prefixY[i - 1]
+    result = Record()
+    for pt in P:
+        x, y = pt
+        val = prefixX[x - 1] + prefixY[y - 1] - len(P) + 1
+        if val > result.maximised:
+            result.pt = pt
+            result.maximised = val
+    return result.maximised
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-
 runtests( dominance, all_tests = True )
-
